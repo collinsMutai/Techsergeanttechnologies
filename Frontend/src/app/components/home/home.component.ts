f80aca4e60372c19db74ea47012608a32e23efe6
Collins Mutai <collinsfrontend@gmail.com>
2025-03-03 18:49:22 +0300

    ui

--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ImageSliderComponent } from '../image-slider/image-slider.component';
+import { NavComponent } from '../nav/nav.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ImageSliderComponent],
+  imports: [ImageSliderComponent, NavComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
