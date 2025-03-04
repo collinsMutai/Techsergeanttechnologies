6e53bd7be693f1529a28b60426e11735ab3aca13
Collins Mutai <collinsfrontend@gmail.com>
2025-03-04 14:53:01 +0300

    ui

--- Frontend/src/app/components/nav/nav.component.ts
+++ Frontend/src/app/components/nav/nav.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
-  standalone: true,
-  imports: [],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrls: ['./nav.component.css'],
+  standalone: true,
+  imports: [CommonModule],
 })
 export class NavComponent {
+  mobileMenuOpen = false;
 
+  toggleMobileMenu() {
+    console.log('clicked');
+
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
+
+  navigateToSection(sectionId: string) {
+    // Close the mobile menu
+    this.mobileMenuOpen = false;
+
+    // Scroll to the section
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
