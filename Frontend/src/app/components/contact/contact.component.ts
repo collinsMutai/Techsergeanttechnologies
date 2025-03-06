2c1bc98db42b79357fe11b8350afa64f161d62aa
Collins Mutai <collinsfrontend@gmail.com>
2025-03-06 23:42:57 +0300

    ui

--- Frontend/src/app/components/contact/contact.component.ts
+++ Frontend/src/app/components/contact/contact.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
 export class ContactComponent {
+  contactForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.contactForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(10)]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+      // You can add logic to send form data to your backend
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -4,11 +4,12 @@ import { NavComponent } from '../nav/nav.component';
 import { FooterComponent } from '../footer/footer.component';
 import { AboutComponent } from "../about/about.component";
 import { ServicesComponent } from "../services/services.component";
+import { ContactComponent } from "../contact/contact.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ImageSliderComponent, NavComponent, FooterComponent, AboutComponent, ServicesComponent],
+  imports: [ImageSliderComponent, NavComponent, FooterComponent, AboutComponent, ServicesComponent, ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
