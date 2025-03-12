d7d414187f64a68ebd5def3b33225fa0398ca7fa
Collins Mutai <collinsfrontend@gmail.com>
2025-03-12 13:29:23 +0300

    ui

--- Frontend/src/app/components/contact/contact.component.ts
+++ Frontend/src/app/components/contact/contact.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { environment } from '../../../environments/environment.prod';
+import * as emailjs from 'emailjs-com'; 
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
 export class ContactComponent {
+  contactForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.contactForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(10)]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      const formData = this.contactForm.value;
+
+      // Sending the form data to EmailJS
+      this.sendEmail(formData);
+    }
+  }
+
+  // Send the email using EmailJS
+  sendEmail(formData: any): void {
+    const serviceId = environment.emailjs.serviceId; // Get the service ID from environment
+    const templateId = environment.emailjs.templateId; // Get the template ID from environment
+    const userId = environment.emailjs.userId; // Get the user ID from environment
+
+    const templateParams = {
+      from_name: formData.name,
+      from_email: formData.email,
+      message: formData.message,
+    };
+
+    emailjs.send(serviceId, templateId, templateParams, userId).then(
+      (response) => {
+        console.log('Email sent successfully:', response);
+        alert('Thank you for contacting us!');
+      },
+      (error) => {
+        console.error('Email sending error:', error);
+        alert('Something went wrong. Please try again.');
+      }
+    );
+  }
 }
